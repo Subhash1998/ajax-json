895a6f6819ae332448f1cba57cc35ad905517d5e
Subhash1998 <30298270+Subhash1998@users.noreply.github.com>
2018-03-01 21:58:11 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,6 @@
 var info = document.getElementById('info');
 var btn = document.getElementById('btn');
+document.getElementById('myInput').style.display='none';
 btn.addEventListener('click',function(){
 
 		var xhttp = new XMLHttpRequest();
@@ -17,14 +18,37 @@ var i=0;
 function renderHTML(data){
 	var htmlString = "";
 
-	if(i<data.length){
-		htmlString += "<tr><td>" + data[i].name + "</td><td>" + data[i].branch + "</td></tr>";
-		i++;
+	for(i=0;i<data.length;i++){
+		htmlString += "<tr><td>" + data[i].name + "</td><td>" + data[i].branch + data.length+ "</td></tr>";
 	}
 	if(i==data.length)
 	{
-		htmlString += "<p>Total records : " + i +"</p>"
+		htmlString += "<p>Total records : " + i + "</p>"
 		i++;
+		document.getElementById('btn').style.display='none';
+		document.getElementById('myInput').style.display='block';
+
 	}
 	info.insertAdjacentHTML('beforeend' , htmlString);
+}
+
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("info");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
 }
